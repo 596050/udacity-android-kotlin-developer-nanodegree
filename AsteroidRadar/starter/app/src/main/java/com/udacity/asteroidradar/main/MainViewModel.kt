26447c5e73d97cadbd9838b22388f3af51bb4cd7
T <thomas.price@sky.uk>
2021-11-23 22:07:56 +0000

    Add item row

--- AsteroidRadar/starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ AsteroidRadar/starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -3,4 +3,18 @@ package com.udacity.asteroidradar.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+//    val asteroids = mutableListOf<Asteroid>()
+//    init {
+//        for (i in 0 until 100) {
+//            asteroid = Asteroid()
+//            asteroid.id = i;
+//            asteroid.codename = "Asteroid #$i";
+//            asteroid.closeApproachDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+//            asteroid.absoluteMagnitude = i % 2;
+//            asteroid.estimatedDiameter = Random.nextDouble();
+//            asteroid.relativeVelocity = Random.nextDouble();
+//            asteroid.distanceFromEarth = Random.nextDouble();
+//            asteroid.isPotentiallyHazardous = i % 2 == 0;
+//        }
+//    }
 }
