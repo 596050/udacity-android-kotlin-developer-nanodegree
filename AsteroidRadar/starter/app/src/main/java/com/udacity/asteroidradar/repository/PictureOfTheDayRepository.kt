5e6857cab5a292b9cd16a668d3b382af4659e3a4
Shrouded Shrew <shroudedshrew@gmail.com>
2021-12-23 23:35:54 +0000

    Remove PictureOfTheDayRepository

--- AsteroidRadar/starter/app/src/main/java/com/udacity/asteroidradar/repository/PictureOfTheDayRepository.kt
+++ AsteroidRadar/starter/app/src/main/java/com/udacity/asteroidradar/repository/PictureOfTheDayRepository.kt
@@ -1,28 +1,28 @@
 import android.util.Log
 import androidx.lifecycle.LiveData
 import com.udacity.asteroidradar.PictureOfDay
-import com.udacity.asteroidradar.api.PictureOfTheDayService
+//import com.udacity.asteroidradar.api.PictureOfTheDayService
 import com.udacity.asteroidradar.database.NasaDao
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class PictureOfTheDayRepository(
-    private val nasaDao: NasaDao,
-    private val service: PictureOfTheDayService
-) {
-    fun getAsteroidImageOfTheDayLiveData(): LiveData<PictureOfDay> = nasaDao.getPictureOfTheDay()
-
-    suspend fun save() {
-        GlobalScope.launch {
-            val response = service.getPictureOfTheDay()
-            Log.i("response", response.toString())
-            if (response != null) {
-                nasaDao.createPictureOfTheDay(
-                    PictureOfDay(
-                        response.title, response.url, response.media_type
-                    )
-                )
-            }
-        }
-    }
-}
+//class PictureOfTheDayRepository(
+//    private val nasaDao: NasaDao,
+//    private val service: PictureOfTheDayService
+//) {
+//    fun getAsteroidImageOfTheDayLiveData(): LiveData<PictureOfDay> = nasaDao.getPictureOfTheDay()
+//
+//    suspend fun save() {
+//        GlobalScope.launch {
+//            val response = service.getPictureOfTheDay()
+//            Log.i("response", response.toString())
+//            if (response != null) {
+//                nasaDao.createPictureOfTheDay(
+//                    PictureOfDay(
+//                        response.title, response.url, response.media_type
+//                    )
+//                )
+//            }
+//        }
+//    }
+//}
